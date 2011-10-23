e0e8d559ee1742dcd9ab3e0e9e4d286e07f048ec
Cam Pedersen <diffference@gmail.com>
2011-10-22 23:49:23 -0400

    resizable!

--- earth.js
+++ earth.js
@@ -6,7 +6,7 @@
  */
 
 var fs = require('fs'),
-    window = require('tty').getWindowSize(1),
+    tty = require('tty'),
     colors = require('colors');
 
 function write(data){
@@ -18,31 +18,34 @@ var earth = {
   escape: '\033[',
   frames: [],
   frame: 0,
-  height: Number(window[0]),
-  width: Number(window[1]),
 
   clear: function(){
     write(earth.escape + '2J');
   },
   pos: function(x, y){
-    write(earth.escape + String(y) + ';' + String(x) + 'f');
+    if (x && y) write(earth.escape + String(y) + ';' + String(x) + 'f');
   },
 
   renderFrame: function(){
+    var window = tty.getWindowSize(1),
+        width = window[1],
+        height = window[0];
     earth.clear();
+    //earth.pos(Math.floor(width / 2) - 2, 5);
+    //write('ecto'.rainbow);
     var frame = earth.frames[earth.frame];
     var rows = frame.split('\n');
     for (var i in rows) {
       var row = rows[i].trim().split('');
       for (var j in row) {
-        var x = Math.floor(earth.width / 2) + Number(j) - Math.floor(row.length / 2),
-            y = Math.ceil(earth.height / 2) + Number(i) - Math.ceil(rows.length / 2);
+        var x = Math.floor(width / 2) + Number(j) - Math.floor(row.length / 2),
+            y = Math.ceil(height / 2) + Number(i) - Math.ceil(rows.length / 2) + 1;
         earth.pos(x, y);
         if (row[j] == ' ') console.log('.'.blue);
         else write(row[j].green);
       }
     }
-    earth.pos(earth.width, earth.height);
+    earth.pos(width, height);
   },
 
   load: function(cb){
